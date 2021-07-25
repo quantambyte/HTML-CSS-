173980eb665073b6375ad0d40d71e214978e7f2a
Uzair Asif <44237563+uzair-asif@users.noreply.github.com>
2021-07-25 05:00:33 -0700

    Expanding Cards
    Project from 50 Projects in 50 Days

---
+++ Expanding Cards/index.js
@@ -0,0 +1,16 @@
+// selecting all panels
+const panels = document.querySelectorAll(".panel");
+
+// adding event listener
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
+
+const removeActiveClasses = () => {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+};
